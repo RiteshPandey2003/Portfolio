7a97261c0e9cda614cba9f63f7859cea8d18d2fa
Ritesh Pandey <115467698+RiteshPandey2003@users.noreply.github.com>
2023-10-24 14:49:15 +0530

    Update page.tsx

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -24,24 +24,6 @@ export default async function Guestbook() {
                 <Certificates />
                 <Skills/>
             </div>
-            <div className="space-y-2 pt-16 pb-8 md:space-y-5">
-                <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-                    Guestbox
-                </h1>
-            </div>
-            <div className="w-full">
-                <div className="max-w-[500px] mx-auto mt-8">
-{/*                     <Form /> */}
-                    <div className="flex flex-col space-y-2">
-{/*                         {data.map((entry) => {
-                            <div key={entry.id} className="w-full text-sm break-words" >
-                                {entry.message}
-                            </div>
-                        })} */}
-                    </div>
-
-                </div>
-            </div>
 
         </div>
     )
