d40dda4fa8843a12ceeeb7b55ea4a674e26a0ab7
Ritesh Pandey <115467698+RiteshPandey2003@users.noreply.github.com>
2023-10-24 14:42:14 +0530

    Update page.tsx

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -3,20 +3,20 @@ import prisma from "../db"
 import Skills from "./Skills";
 import Certificates from "./certificates";
 
-async function getEntries() {
-    const data = await prisma.guestbook.findMany({
-        take: 50,
-        orderBy: {
-            creates_at: "desc",
-        },
-    });
-    return data;
-}
+// async function getEntries() {
+//     const data = await prisma.guestbook.findMany({
+//         take: 50,
+//         orderBy: {
+//             creates_at: "desc",
+//         },
+//     });
+//     return data;
+// }
 
-export const revalidate = 140;
+// export const revalidate = 140;
 
 export default async function Guestbook() {
-    const data = await getEntries();
+    // const data = await getEntries();
     return (
         <div className="divide-y divide-gray-200 dark:divide-gray-700">
             <div className="space-y-2 pt-6 pb-8 md:space-y-5">
@@ -33,7 +33,7 @@ export default async function Guestbook() {
                 <div className="max-w-[500px] mx-auto mt-8">
                     <Form />
                     <div className="flex flex-col space-y-2">
-                        {/* {data.map((entry) => {
+{/*                         {data.map((entry) => {
                             <div key={entry.id} className="w-full text-sm break-words" >
                                 {entry.message}
                             </div>
